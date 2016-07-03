65a4ecda3caf6e9464807322a29d00ff4caeb3bf
Luan <luan@luanmuniz.com.br>
2016-07-03 20:28:15 -0300

    Fix argument options

--- index.js
+++ index.js
@@ -8,62 +8,97 @@ var PasswordGenerator = {
 
 	/**
 	 * Generate a random password given the params
-	 * @param {Number} options.size - the total length of the password
-	 * @param {Number|Boolean} options.numbers - The total numbers that the password should have
-	 * @param {Number} options.symbols - The total symbols that the password should have
-	 * @param {Boolean} options.allowUppercase - is uppercase chars allowed?
-	 * @param {Boolean} options.allowRepetintion - is repetited chars allowed?
+	 * @param {Number} size - the total length of the password
+	 * @param {Object} options - Object with the options of configuration
 	 * @return {String} The generated password
 	 */
 
-	generate({ size = 16, numbers = 5, symbols = 5, allowUppercase = true, allowRepetintion = true } = {}) { // eslint-disable-line no-magic-numbers
+	generate() {
 		var finalPassword = '',
 			numberPassword = '',
 			symbolsPassword = '',
-			possibleChars = PasswordGenerator.alphabetChars;
+			possibleChars = PasswordGenerator.alphabetChars,
+			options = PasswordGenerator.mergeOptions(arguments);
 
-		if(allowUppercase) {
+		if(options.allowUppercase) {
 			possibleChars += PasswordGenerator.alphabetChars.toUpperCase();
 		}
 
-		for(let i = 0; i < size; i++) {
-			finalPassword += PasswordGenerator.generateNextChar(possibleChars, finalPassword, allowRepetintion);
+		for(let i = 0; i < options.size; i++) {
+			finalPassword += PasswordGenerator.generateNextChar(possibleChars, finalPassword, options);
 		}
 
-		if(numbers > 0) {
-			finalPassword = finalPassword.substring(0, (finalPassword.length - numbers));
+		if(options.numbers > 0) {
+			finalPassword = finalPassword.substring(0, (finalPassword.length - options.numbers));
 
-			for(let i = 0; i < numbers; i++) {
-				numberPassword += PasswordGenerator.generateNextChar(PasswordGenerator.numbersChars, numberPassword, allowRepetintion);
+			for(let i = 0; i < options.numbers; i++) {
+				numberPassword += PasswordGenerator.generateNextChar(PasswordGenerator.numbersChars, numberPassword, options);
 			}
+
+			finalPassword = finalPassword + numberPassword;
 		}
 
-		if(symbols > 0) {
-			finalPassword = finalPassword.substring(0, (finalPassword.length - symbols));
+		if(options.symbols > 0) {
+			finalPassword = finalPassword.substring(0, (finalPassword.length - options.symbols));
 
-			for(let i = 0; i < symbols; i++) {
-				symbolsPassword += PasswordGenerator.generateNextChar(PasswordGenerator.symbolsChars, symbolsPassword, allowRepetintion);
+			for(let i = 0; i < options.symbols; i++) {
+				symbolsPassword += PasswordGenerator.generateNextChar(PasswordGenerator.symbolsChars, symbolsPassword, options);
 			}
+
+			finalPassword = finalPassword + symbolsPassword;
 		}
 
-		finalPassword = finalPassword + numberPassword + symbolsPassword;
 		finalPassword = PasswordGenerator.shuffleString(finalPassword);
 
 		return finalPassword;
 	},
 
 	/**
+	 * Merge default options with Users options
+	 * @param {Number|Object} userOptions Arguments passing in the function generate
+	 * @return {Object} Object with the final options
+	 */
+	mergeOptions(userOptions) {
+		var userObjOptions = userOptions[0],
+			defaultValues = {
+				size: 16,
+				numbers: 5,
+				symbols: 5,
+				allowUppercase: true,
+				allowRepetintion: false
+			};
+
+		if(typeof userObjOptions === 'number') {
+			defaultValues.size = userObjOptions;
+		}
+
+		if(userOptions[1] && userOptions[1] === Object(userOptions[1])) {
+			userObjOptions = userOptions[1];
+		}
+
+		if( userObjOptions === Object(userObjOptions) ) {
+			defaultValues.size = (userObjOptions.size || defaultValues.size);
+			defaultValues.numbers = (userObjOptions.numbers || defaultValues.numbers);
+			defaultValues.symbols = (userObjOptions.symbols || defaultValues.symbols);
+			defaultValues.allowUppercase = (userObjOptions.allowUppercase || defaultValues.allowUppercase);
+			defaultValues.allowRepetintion = (userObjOptions.allowRepetintion || defaultValues.allowRepetintion);
+		}
+
+		return defaultValues;
+	},
+
+	/**
 	 * Generate the next char for the password string
 	 * @param {String} possibleChars - A string containing all the chars allowed in the password
 	 * @param {String} hash - The actual password
-	 * @param {Boolean} allowRepetintion - Boolean indicating if we can have repetite chars in the password
+	 * @param {Object} options - Object with the module configuration
 	 * @return {String} The next char for the password
 	*/
-	generateNextChar(possibleChars, hash, allowRepetintion) {
+	generateNextChar(possibleChars, hash, options) {
 		let nextChar = possibleChars.charAt(Math.floor(Math.random() * possibleChars.length));
 
-		if(!allowRepetintion && PasswordGenerator.checkForRepetintion(hash, nextChar)) {
-			nextChar = PasswordGenerator.generateNextChar(possibleChars, hash, allowRepetintion);
+		if(!options.allowRepetintion && PasswordGenerator.checkForRepetintion(hash, nextChar) && (options.numbers <= PasswordGenerator.numbersChars.length &&	options.symbols <= PasswordGenerator.symbolsChars.length)) {
+			nextChar = PasswordGenerator.generateNextChar(possibleChars, hash, options);
 		}
 
 		return nextChar;
@@ -81,15 +116,14 @@ var PasswordGenerator = {
 
 	/**
 	 * Shuffle the String with all the chars for the password, that way we have the exact amount of each type of chars in the password
-	 * @param  {String} allStrings - the string to be suffled
+	 * @param  {String} finalPassword - the string to be suffled
 	 * @return {String} shuffled string
 	 */
-	shuffleString(allStrings) {
-		return allStrings
+	shuffleString(finalPassword) {
+		return finalPassword
 			.split('')
 			.sort(() => 0.5 - Math.random()) // eslint-disable-line no-magic-numbers
-			.join('')
-			;
+			.join('');
 	}
 
 };
